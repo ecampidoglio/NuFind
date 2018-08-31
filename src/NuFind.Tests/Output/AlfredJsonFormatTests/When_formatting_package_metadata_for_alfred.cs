b184acbfbae2d2a34674faf0aac277d63d99619f
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2018-08-31 15:26:40 +0200

    Adds a few tests for the Alfred output format

---
+++ src/NuFind.Tests/Output/AlfredJsonFormatTests/When_formatting_package_metadata_for_alfred.cs
@@ -0,0 +1,53 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using NuFind.Output;
+using NuFind.Search;
+using Xunit;
+
+namespace NuFind.Tests.Output.AlfredJsonFormatTests
+{
+    public class When_formatting_package_metadata_for_alfred
+    {
+        [Theory, InlineAutoData]
+        public void Should_return_a_string_containing_the_package_id_as_title(
+            PackageMetadata package,
+            AlfredJsonFormat sut)
+        {
+            var result = sut.Render(new[] { package });
+
+            result.Should().Contain($"\"title\":\"{package.Id}\"");
+        }
+
+        [Theory, InlineAutoData]
+        public void Should_return_a_string_containing_the_package_version_and_authors_as_subtitle(
+            PackageMetadata package,
+            AlfredJsonFormat sut)
+        {
+            var result = sut.Render(new[] { package });
+
+            result.Should().Contain($"\"subtitle\":\"{package.Version} • {package.Authors}\"");
+        }
+
+        [Theory, InlineAutoData]
+        public void Should_return_a_string_containing_the_msbuild_package_reference_as_argument(
+            PackageMetadata package,
+            AlfredJsonFormat sut)
+        {
+            var result = sut.Render(new[] { package });
+
+            result
+                .Should()
+                .Contain($"\"arg\":\"<PackageReference Include=\\\"{package.Id}\\\" Version=\\\"{package.Version}\\\" />\"");
+        }
+
+        [Theory, InlineAutoData]
+        public void Should_return_a_string_containing_the_package_id_for_autocomplete(
+            PackageMetadata package,
+            AlfredJsonFormat sut)
+        {
+            var result = sut.Render(new[] { package });
+
+            result.Should().Contain($"\"autocomplete\":\"{package.Id}\"");
+        }
+    }
+}
