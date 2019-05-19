46e9032e58d66bfee99f7807d7824d49039a0890
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-05-19 12:37:12 +0200

    Builds using the .NET Core 2.1 SDK in the Bash bootstrapper

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ########################################################################################
-# This is a Cake bootstrapper script for Linux and macOS using .NET Core 2.0
+# This is a Cake bootstrapper script for Linux and macOS using .NET Core 2.1
 # Inspired by
 # https://adamhathcock.blog/2017/07/12/net-core-on-circle-ci-2-0-using-docker-and-cake/
 ########################################################################################
@@ -19,7 +19,7 @@ fi
 # Install Cake.CoreCLR
 if [ ! -f "$CAKE_DLL" ]; then
     echo -e "\e[92mInstalling Cake $CAKE_VERSION for .NET Core in '$TOOLS_DIR'\e[0m"
-    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.0</TargetFramework></PropertyGroup></Project>" > $TOOLS_PROJ
+    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.1</TargetFramework></PropertyGroup></Project>" > $TOOLS_PROJ
     dotnet add $TOOLS_PROJ package Cake.CoreCLR -v $CAKE_VERSION --package-directory $TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION
 fi
 
